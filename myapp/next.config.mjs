7c9977c7665d73d398555161d4aff344f959533f
4R5T <maxmeetsli@gmail.com>
2024-12-30 18:42:08 -0500

    origin push

---
+++ myapp/next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV = 'production';
+
+const nextConfig = {
+    basePath : isProd ? '/myweb' : '',
+    output : 'export',
+    distDir : 'dist',
+    images : {
+        unoptimized :true,
+    },
+};
+
+export default nextConfig;
