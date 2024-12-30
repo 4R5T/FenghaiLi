87a20e88a565bd5e0b7145ec48c1df663758e77a
4R5T <maxmeetsli@gmail.com>
2024-12-30 18:55:36 -0500

    update yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV = 'production';
 
 const nextConfig = {
-    basePath : isProd ? '/myweb' : '',
+    basePath : '/myweb',
     output : 'export',
     distDir : 'dist',
     images : {
